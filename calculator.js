929f84e2949e4b86c089025d4622d18097354a31
TipDevs <tipdevs@gmail.com>
2025-01-28 02:42:28 +0100

    I added the basic calculation functions which includes addition, subtraction, multiplication and division

---
+++ calculator.js
@@ -0,0 +1,16 @@
+//Addition
+function add(num1,  num2) {
+    return num1 + num2;
+}
+//Subtraction
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+//Multiplication
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+//Division
+function division(num1, num2) {
+    return num1 / num2;
+}
